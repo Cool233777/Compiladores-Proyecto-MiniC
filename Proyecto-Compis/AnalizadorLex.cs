7c4365f84d28c3a8ac2d93c41347e406ca5da244
aleve <alevelasquezmorales@gmail.com>
2020-08-19 16:33:54 -0600

    se agregó código para el analizador, algunas variables, vector y lista

--- Proyecto-Compis/AnalizadorLex.cs
+++ Proyecto-Compis/AnalizadorLex.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Text.RegularExpressions;
 namespace Proyecto_Compis
 {
     class AnalizadorLex
     {
+        Regex regex;
+        StringBuilder pattern;
+        bool compilar;
+        List<string> TNombres;
+        int[] numeros;
+
+        public AnalizadorLex()
+        {
+            compilar = true;
+            TNombres = new List<string>();
+        }
     }
 }
