7cceb0d0a04a573cf7e3a9c43b2761a9a0d863dd
aleve <alevelasquezmorales@gmail.com>
2020-08-19 21:18:10 -0600

    cambios en el código como clase nueva creada y parte de código en la misma

---
+++ Proyecto-Compis/Reset.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Compis
+{
+    class Reset
+    {
+        AnalizadorLex lexer;
+        public void reset()
+        {
+            lexer.gex = null;
+            lexer.pattern = null;
+            lexer.compilar = true;
+            lexer.TNombres.Clear();
+            lexer.numeros = null;
+        }
+        
+    }
+}
