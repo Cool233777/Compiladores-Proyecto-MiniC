72510e6fcd1ce9ea06a5ba2156227e130d88acea
aleve <alevelasquezmorales@gmail.com>
2020-08-20 15:11:24 -0600

    Añadi otras variables que se utilizaran en el lexer al igual que un metodo para empezar con el conteo de lineas

--- Proyecto-Compis/AnalizadorLex.cs
+++ Proyecto-Compis/AnalizadorLex.cs
@@ -4,20 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using Proyecto_Compis.Properties;
 namespace Proyecto_Compis
 {
-    class AnalizadorLex
+    public class AnalizadorLex
     {
-        Regex regex;
-        StringBuilder pattern;
-        bool compilar;
-        List<string> TNombres;
-        int[] numeros;
+        public Regex gex;
+        public StringBuilder pattern;
+        public List<string> TNombres;
+        public int[] numeros;
+        public bool compilar;
 
         public AnalizadorLex()
         {
             compilar = true;
             TNombres = new List<string>();
+            
         }
+
+
+
+        //Conteo de lineas
+        public int contarLineas(int indice,ref int lineaComienzo, string token)
+        {
+            int linea = 0;
+            for (int i=0; i<token.Length;i++)
+                if(token[i]=='\n')
+                {
+                    linea++;
+                    lineaComienzo = indice + i + 1;
+                }
+            return linea;
+        }
+        
+
+
     }
 }
