f424ea94d60cfec15d83970ec1aa4a464e22f393
Cool232777 <cool232777@gmail.com>
2020-08-24 21:40:21 -0600

    Se agrego método para agregar y reconocer tokens, el patron de los tokens también fue agregado

--- Proyecto-Compis/AnalizadorLex.cs
+++ Proyecto-Compis/AnalizadorLex.cs
@@ -19,24 +19,52 @@ namespace Proyecto_Compis
         {
             compilar = true;
             TNombres = new List<string>();
-            
+
         }
 
+        //Método para agregar las reglas y reconocer los tokens
+        public void NuevaReglaDeTokens(string patron_Nuevo, string token_Nombre, bool ignorar = false)
+        {
+            if (string.IsNullOrWhiteSpace(token_Nombre))
+            {
+                throw new ArgumentException(string.Format("{0} no es un nombre válido.", token_Nombre));
+            }
 
+            if (string.IsNullOrEmpty(patron_Nuevo))
+            {
+                throw new ArgumentException(string.Format("El patrón {0} no es válido.", patron_Nuevo));
+            }
+
+            if (pattern == null)
+            {
+                pattern = new StringBuilder(string.Format("(?<{0}>{1})", token_Nombre, patron_Nuevo));
+            }
+            else
+            {
+                pattern.Append(string.Format("|(?<{0}>{1})", token_Nombre, patron_Nuevo));
+            }
+
+            if (!ignorar)
+            { 
+                TNombres.Add(token_Nombre); 
+            }          
+
+            compilar = true;
+        }      
 
         //Conteo de lineas
-        public int contarLineas(int indice,ref int lineaComienzo, string token)
+        public int contarLineas(int indice, ref int lineaComienzo, string token)
         {
             int linea = 0;
-            for (int i=0; i<token.Length;i++)
-                if(token[i]=='\n')
+            for (int i = 0; i < token.Length; i++)
+                if (token[i] == '\n')
                 {
                     linea++;
                     lineaComienzo = indice + i + 1;
                 }
             return linea;
         }
-        
+
 
 
     }
--- Proyecto-Compis/Form1.cs
+++ Proyecto-Compis/Form1.cs
@@ -15,7 +15,8 @@ namespace Proyecto_Compis
     public partial class Form1 : Form
     {
              
-        public Dictionary<int, string> DicReservadas = new Dictionary<int, string>() { {1,  "void" }, {2, "public" }, { 3, "int" }, { 4, "double" }, { 5, "bool" }, { 6, "string" }, { 7, "const" }, { 8, "if" }, { 9, "null" }, { 10, "if" }, { 11, "else" }, { 12, "return" }, { 13, "New" }, { 14, "Console" }, { 15, "WriteLine" }, { 16, "for" }, { 17, "while" }, { 18, "break" }, { 19, "class" }, { 20, "interface" }, { 21, "foreach" }, { 22, "NewArray" }, { 23, "class" }, { 24, "this" }, }; 
+        public Dictionary<int, string> DicReservadas = new Dictionary<int, string>() { {1,  "void" }, {2, "public" }, { 3, "int" }, { 4, "double" }, { 5, "bool" }, { 6, "string" }, { 7, "const" }, { 8, "if" }, { 9, "null" }, { 10, "if" }, { 11, "else" }, { 12, "return" }, { 13, "New" }, { 14, "Console" }, { 15, "WriteLine" }, { 16, "for" }, { 17, "while" }, { 18, "break" }, { 19, "class" }, { 20, "interface" }, { 21, "foreach" }, { 22, "NewArray" }, { 23, "class" }, { 24, "this" }, };
+        public AnalizadorLex AnalizadorLexico = new AnalizadorLex();
         public Form1()
         {
             InitializeComponent();
@@ -34,8 +35,25 @@ namespace Proyecto_Compis
                 {
                     listaArchivoOriginal.Add(lector.ReadLine());
                 }
+                label2.Text = archivo.Name;
                 archivo.Close(); lector.Close();
             }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Introduccir reglas para reconcoer los tokens
+            AnalizadorLexico.NuevaReglaDeTokens(@"\s+", "ESPACIO", true);
+            AnalizadorLexico.NuevaReglaDeTokens(@"\b[_a-zA-Z][\w]*\b", "IDENTIFICADOR");
+            AnalizadorLexico.NuevaReglaDeTokens("\".*?\"", "CADENA");
+            AnalizadorLexico.NuevaReglaDeTokens(@"'\\.'|'[^\\]'", "CARACTER");
+            AnalizadorLexico.NuevaReglaDeTokens("//[^\r\n]*", "COMENTARIO1");
+            AnalizadorLexico.NuevaReglaDeTokens("/[*].*?[*]/", "COMENTARIO2");
+            AnalizadorLexico.NuevaReglaDeTokens(@"\d*\.?\d+", "NUMERO");
+            AnalizadorLexico.NuevaReglaDeTokens(@"[\(\)\{\}\[\];,]", "DELIMITADOR");
+            AnalizadorLexico.NuevaReglaDeTokens(@"[\.=\+\-/*%]", "OPERADOR");
+            AnalizadorLexico.NuevaReglaDeTokens(@">|<|==|>=|<=|!", "COMPARADOR");
         }
     }
 }
