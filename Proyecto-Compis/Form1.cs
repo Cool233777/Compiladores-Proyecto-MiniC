0f9e85be43bfee17b6445f5997dbaaf09c29b8a8
aleve <alevelasquezmorales@gmail.com>
2020-08-28 16:49:52 -0600

    Cambio en dos ER

--- Proyecto-Compis/Form1.cs
+++ Proyecto-Compis/Form1.cs
@@ -48,9 +48,9 @@ namespace Proyecto_Compis
             Analizador_Lexico.NuevaReglaDeTokens("//[^\r\n]*", "COMENTARIO_LINEAL");
             Analizador_Lexico.NuevaReglaDeTokens("/[*].*?[*]/", "COMENTARIO_PARRAFO");
             Analizador_Lexico.NuevaReglaDeTokens(@"-*[0-9]+\.[0-9]*([eE][+-]?)?[0-9]+", "DECIMAL");
-            Analizador_Lexico.NuevaReglaDeTokens(@"([0][x|X])([0-9]|[a-f][A-F])*", "HEXADECIMAL");
+            Analizador_Lexico.NuevaReglaDeTokens(@"([0][x|X])([0-9]|[a-f]|[A-F])*", "HEXADECIMAL");
             Analizador_Lexico.NuevaReglaDeTokens(@"-*[0-9]+", "NUMERO");
-            Analizador_Lexico.NuevaReglaDeTokens(@"[\(\)\{\}\[\];,]", "DELIMITADOR");
+            Analizador_Lexico.NuevaReglaDeTokens(@"[\(\)\{\}\[\];,:]", "DELIMITADOR");
             Analizador_Lexico.NuevaReglaDeTokens(@"[\.=\+\-/*%]", "OPERADOR");
             Analizador_Lexico.NuevaReglaDeTokens(@">|<|==|>=|<=|!", "COMPARADOR");
 
