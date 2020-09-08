7c940732c8c8ba25d0ad70861d0e21c4dafbe490
aleve <alevelasquezmorales@gmail.com>
2020-09-07 22:44:30 -0600

    Añadi dos void, y el comienzo de un switch

--- Proyecto-Compis/AnalizadorSintactico.cs
+++ Proyecto-Compis/AnalizadorSintactico.cs
@@ -13,7 +13,7 @@ namespace Proyecto_Compis
         public string LookAhead;
         public List<PropiedadesDePalabras> Tokens;
         public int cont = 0;
-        
+        Stack<PropiedadesDePalabras> Expr = new Stack<PropiedadesDePalabras>();
         public AnalizadorSintactico(AnalizadorLex Lex, List<PropiedadesDePalabras>Tokns)
         {
             Regex = Lex.REGEX;
@@ -73,8 +73,65 @@ namespace Proyecto_Compis
         public void Parse_Expression()
         {
             //ver todo lo que esta adentro de la expresion
-        }
 
+            var contador = cont;
+            while (Tokens[contador].Cadena!=")")
+            {
+                Expr.Push(Tokens[contador]);
+                contador++;
+            }
+            var Cadena_A_Comparar = 1;
+            switch (Cadena_A_Comparar)
+            {
+                //case 1:
+                //if ((Expr.Peek().Nombre == "IDENTIFICADOR") || (Parse_Expression()+"." + "IDENTIFICADOR" == "R"))
+                // {
+
+                //break;
+                case 1:
+                    Parse_Constant();
+                    Cadena_A_Comparar = 2;
+                    break;
+                case 2:
+                    Parse_LValue();
+                    break;
+                default:
+                    break;
+ 
+            }
+        }
+        public void Parse_LValue()
+        {
+            //dudas
+        }
+        public void Parse_Constant()
+        {
+            if(Tokens[cont].Nombre == "NUMERO")
+            {
+
+            }
+            else if 
+                (Tokens[cont].Nombre=="DECIMAL")
+            {
+
+            }
+            else if (Tokens[cont].Nombre=="RESERVADO" && Tokens[cont].Cadena == "bool")
+            {
+
+            }
+            else if (Tokens[cont].Nombre == "RESERVADO" && Tokens[cont].Cadena == "string")
+            {
+
+            }
+            else if (Tokens[cont].Nombre == "RESERVADO" && Tokens[cont].Cadena == "null")
+            {
+
+            }
+            else
+            {
+                //error de sintaxis, porque estos son los parámetros que recibe el if obligatorio 
+            }
+        }
         public void Parse_Statement()
         {
             if (LookAhead == "if")
