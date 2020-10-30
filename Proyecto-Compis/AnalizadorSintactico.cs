2d12b2f55bb22b09342488e6a7c26608eb2cbe2e
Cool232777 <cool232777@gmail.com>
2020-10-29 23:24:50 -0600

    Se cambio el boton del diseño

--- Proyecto-Compis/AnalizadorSintactico.cs
+++ Proyecto-Compis/AnalizadorSintactico.cs
@@ -347,7 +347,7 @@ namespace Proyecto_Compis
             CrearDicNoTerminales();
             var Simbolos = new string[89] { ";", "id", "const", "int", "double", "bool", "string", "[]", "(", ")", "void", ",", "class", "{", "}", ":", "interface", "if", "else", "while", "for", "return", "break", "Console", ".", "WriteLine", "=", "==", "&&", "<", "<=", "+", "*", "%", "-", "!", "this", "New", "intConstant", "doubleConstant", "boolConstant", "stringConstant", "null", "$", "S'", "Program", "Decl", "VariableDecl", "Variable", "ConstDecl", "ConstType", "Type", "Type_P", "Type_R", "FunctionsDecl", "Formals", "Formals_P", "ClassDecl", "ClassDecl_P", "ClassDecl_R", "ClassDecl_O", "ClassDecl_Q", "Field", "InterfaceDecl", "InterfaceDecl_P", "Prototype", "StmtBlock", "StmtBlock_P", "StmtBlock_R", "StmtBlock_O", "Stmt", "Stmt_P", "IfStmt", "IfStmt_P", "WhileStmt", "ForStmt", "ReturnStmt", "BreakStmt", "PrintStmt", "PrintStmt_P", "Expr", "ExprOr", "ExprOrP", "ExprAnd", "ExprAndP", "ExprEquals", "ExprEqualsP", "ExprComp", "ExprCompP" };
             //var Archivo = new FileStream(@"D:\Descargas\SLR.txt", FileMode.Open);
-            var Archivo = new FileStream(Path.GetFullPath("SLR.txt"), FileMode.Open);
+            var Archivo = new FileStream(@"D:\Escritorio\SLR.txt", FileMode.Open);
             var Lector = new StreamReader(Archivo);
             var PosEstados = 0;
             while (!Lector.EndOfStream)
--- Proyecto-Compis/Form1.Designer.cs
+++ Proyecto-Compis/Form1.Designer.cs
@@ -74,12 +74,12 @@
             // 
             this.button2.BackColor = System.Drawing.Color.PeachPuff;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(295, 148);
+            this.button2.Location = new System.Drawing.Point(294, 134);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(142, 58);
+            this.button2.Size = new System.Drawing.Size(173, 72);
             this.button2.TabIndex = 3;
-            this.button2.Text = "Analizador léxico";
+            this.button2.Text = "Analizador sintáctico y léxico";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
